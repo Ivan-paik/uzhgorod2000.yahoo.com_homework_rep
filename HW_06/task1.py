f0968345ce2809e93965de9e77e2a8ecd0cb5cb7
Ivan-paik <126617858+Ivan-paik@users.noreply.github.com>
2023-03-22 23:11:39 +0200

    HW-06 - task1, finish version

--- HW_06/task1.py
+++ HW_06/task1.py
@@ -1,9 +1,20 @@
-a = 2
-b = 3
+user_input = input('Input two number separated by space, please: ')
+
+a = b = ''
+number = 1
+
+# Ручний парсинг рядка для пошуку обох чисел і тільки з тими функціями, які проходили.
+for digit in user_input:
+    if digit != " ":
+        if number == 1 :
+            a = a + digit
+        else:
+            b = b + digit
+    else:
+        number = 2
 
 def Exponentiation (x, y):
     z = x ** y
     return(z)
 
-print(Exponentiation(a, b))
-
+print(f"{a} to the Power of {b} is {Exponentiation(int(a), int(b))}")
