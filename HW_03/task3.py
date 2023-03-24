ad5e48607de08afd30d1b7c8366c9281f429cf29
Ivan-paik <126617858+Ivan-paik@users.noreply.github.com>
2023-03-25 00:37:17 +0200

    Update HW_03/task3.py
    Co-authored-by: Vitalii <webasman@gmail.com>

--- HW_03/task3.py
+++ HW_03/task3.py
@@ -2,7 +2,7 @@ age_in_month = (2022 - 1977) * 12 + 4
 
 # v1 simple the best
 age_in_years = age_in_month // 12
-print('v1: ',age_in_years)
+print('v1: ', age_in_years)
 
 # v2 privedennya tipiv
 age_in_years = int(age_in_month / 12)
