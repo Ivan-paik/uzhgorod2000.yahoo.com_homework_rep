9a5dab4466649295612962a04fffa706b9da710f
Ivan-paik <126617858+Ivan-paik@users.noreply.github.com>
2023-03-25 00:09:36 +0200

    HW-07 - ready

--- HW_07/task1.py
+++ HW_07/task1.py
@@ -1,41 +1,63 @@
+# Testing data.
+# Unique numbers. One man have a few phone numbers.
 phonebook_dict = {
     "+11111111": "Tom",
     "+33333333": "Bob",
     "+55555555": "Alice",
-    "+44444444v": "Tom"
+    "+44444444": "Tom"
 }
-key = "+55555555"
-if key in phonebook_dict:
-    user = phonebook_dict[key]
-#    print(phonebook_dict)
-else:
-    print("Елемент не знайдено")
 
-#print(phonebook_dict)
-# stats: кількість записів
-# show <name>: детальна інформація по імені
-
+# Main program
 while True:
-    print("Press key for continue: s - stats, a - add item, v - view items, n - name list, d - delete by name, x - exit")
+    print("Print command name and press Enter: stats, add, delete, list, show, exit: ")
     operation = input()
-    match operation:
-        case "s": # stats
-            print(len(phonebook_dict))
-        case "a": #  add item
-            add_data_lst = input("Enter the the phone number and name separated by space: ").split()
 
-            phonebook_dict[add_data_lst[0]] = add_data_lst[1]
-            print("Add item complete")
-        case "v": # view items
+    match operation:
+
+        case "stats":
+            print(f"Count of items: {len(phonebook_dict)}")
+
+        case "add":
+            user_input = input("Enter the the phone number and name separated by space: ").split()
+            if user_input[0] in phonebook_dict:
+                print("Same number is present. Can'n add this item")
+            else:
+                phonebook_dict[user_input[0]] = user_input[1]
+                print("Add item complete")
+
+        case "delete": # delete by name !!!
+            user_input = input("Enter the name and press Enter: ")
+            key_list_to_delete = []
+            found_name = False
             for key, value in phonebook_dict.items():
-                print(f"{value} have a phone number {key}")
-        case "n": # name list
+                if user_input == value:
+                    print(f"{value} have a phone number {key}")
+                    key_list_to_delete.append(key)
+                    found_name = True
+            if found_name:
+                for key in key_list_to_delete:
+                    del phonebook_dict[key]
+                print(f"'{user_input}' was deleted")
+            else:
+                print(f"'{user_input}' not found")
+
+        case "list": # list of names
             for value in phonebook_dict.values():
                 print(value)
-        case "d": # delete by name
-            add_data = input("Enter the name: ") #need name
-            del phonebook_dict[add_data]
-        case "x": # exit
+
+        case "show":
+            user_input = input("Enter the name and press Enter: ")
+
+            found_name = False
+            for key, value in phonebook_dict.items():
+                if user_input == value:
+                    print(f"{value} have a phone number {key}")
+                    found_name = True
+            if not found_name:
+                print(f"'{user_input}' not found")
+
+        case "exit":
             break
+
         case _:
             print("Select incorrect option. Try again:")
