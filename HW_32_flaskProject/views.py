2f15bc32c215b2ef4d566e766d581060eafa10f7
Ivan-paik <126617858+Ivan-paik@users.noreply.github.com>
2023-07-31 16:00:15 +0300

    HW_34 - ready! Веб-розробка за допомогою Flask. Part 2

--- HW_32_flaskProject/views.py
+++ HW_32_flaskProject/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import abort, request, redirect
+from flask import abort, request, redirect, render_template, session, url_for
 from random import randint, sample
 
 
@@ -61,132 +61,68 @@ books = [
 
 @app.get('/')
 def home():
-    return '''
-    </br>
-    <h1 style="text-align: center;"><a href="/login">Login</a></h1>
-    <h1 style="text-align: center;"><a href="/users">Users</a></h1>
-    <h1 style="text-align: center;"><a href="/books">Books</a></h1>
-    <h1 style="text-align: center;"><a href="/params">Params</a></h1>
-    '''
-
-##  Basic task 1 was here
-# @app.get('/users')
-# def user_list():
-#     i = randint(0, len(users)) # Total number of users to show. Zero too.
-#     if i > 0:
-#         users_random_list = sample(users, k=i)
-#         users_render = ''.join([
-#             f"{user['user_name']} "
-#             for user in users_random_list
-#         ])
-#     else:
-#         users_render = 'Bingo! Zero users to show'
-#     response = f'''
-#     <h2>Random users list</h2>
-#     <h1> {users_render}</h1>
-#     '''
-#     return response, 200
+    return render_template('home.html', title='Users random list')
 
 
 @app.route('/users')
 def user_list():
     filters = request.args
-    if filters.get('count') == None or int(filters.get('count')) == 0:
-        users_random_list = sample(users, k=randint(1, len(users))) # Random number of users to show. Minimum one)
+    if filters.get('count') == None or int(filters.get('count')) == 0:  # count not specified
+        users_random_list = sample(users, k=randint(1, len(users)))  # Random number of users to show. Minimum one)
+    elif int(filters.get('count')) > len(users):
+        users_random_list = [
+        {
+            'user_name': 'There are not so many users'  # User name as message to showing
+        }]
     else:
-        if int(filters.get('count')) > len(users):
-            return (f'There are not so many users')
-        else:
-            users_random_list = sample(users, k=int(filters.get('count')))
+            users_random_list = sample(users, k=int(filters.get('count')))  # counter below users number
 
-    users_render = ''.join([
-        f"{user['user_name']}</br>"
-        for user in users_random_list
-    ])
-    response = f'''
-           <h1>Random user list</h1>
-           <ul>
-           {users_render}
-           </ul>
-           '''
-    return (f'{response}')
-
-
-##  Basic task 1 was here
-# @app.get('/books')
-# def book_list():
-#     books_random_list = sample(books, k=randint(1, len(books))) # Total number of books to show. Minimum one.
-#     books_render = ''.join([
-#         f"<li>{book['title']}</li>"
-#         for book in books_random_list
-#     ])
-#     response = f'''
-#     <h1>Random books list</h1>
-#     <ul>
-#     {books_render}
-#     </ul>
-#     '''
-#     return response, 200
+    return render_template('users/users.html', title='Users random list', users_list=users_random_list, login_name=session.get('login_name')), 200
 
 
 @app.route('/books')
 def books_list():
     filters = request.args
-    if filters.get('count') == None or int(filters.get('count')) == 0:
-        books_random_list = sample(books, k=randint(1, len(books)))  # Total number of books to show. Minimum one)
+    if filters.get('count') == None or int(filters.get('count')) == 0:  # count not specified
+        books_random_list = sample(books, k=randint(1, len(books)))  # Random number of books to show. Minimum one.
+    elif int(filters.get('count')) > len(books):  # There are not so many books
+        books_random_list = [
+        {
+            'title': 'There are not so many books'  # Book title as message to showing
+        }]
     else:
-        if int(filters.get('count')) > len(books):
-            return (f'There are not so many books')
-        else:
-            books_random_list = sample(books, k=int(filters.get('count')))
+        books_random_list = sample(books, k=int(filters.get('count')))  # counter below books number
 
-    books_render = ''.join([
-        f"<li>{book['title']}</li>"
-        for book in books_random_list
-    ])
-    response = f'''
-           <h1>Random books list</h1>
-           <ul>
-           {books_render}
-           </ul>
-           '''
-    return (f'{response}')
+    contex = {
+        'title': 'Books random list',  # HTML page title
+        'books_random_list': books_random_list,
+        'login_name': session.get('login_name'),
+    }
+    return render_template('books/books.html', **contex), 200
 
 
 @app.get('/users/<int:user_id>')
 def user_details(user_id):
     if user_id % 2 == 0:
-        response = f"{user_id}"
+        contex = {
+            'title': 'User_id',
+            'user_id': user_id,
+            'login_name': session.get('login_name'),
+        }
+        return render_template('users/users_id.html', **contex), 200
     else:
-        abort(404, 'Not Found')
-    return response, 200
+        return render_template('users/users_id.html', title='User not found', user_id='Not found', login_name=session.get('login_name')), 404
 
 
 @app.get('/books/<string:book_title>')
 def book_details(book_title):
-    response = f"{book_title.capitalize()}"
-    return response, 200
+    return render_template('books/books_title.html', book_title=book_title, login_name=session.get('login_name')), 200
 
 
 @app.route('/params')
 def params_detail():
-    filters = request.args
-    response_to_grid = ''
-    for item in filters:
-        response_to_grid += f'''<tr>
-          <td>{item}</td>
-          <td>{filters.get(item)}</td>
-        </tr>'''
-    response = f'''
-    <table width=20%>
-    <tbody>
-    <td>parameter</td>
-    <td>value</td>
-    {response_to_grid}
-    </tbody>
-    </table>
-    '''
-    return (f'{response}')
+    return render_template('params/params.html', params_list=request.args, login_name=session.get('login_name')), 200
+
 
 def rules_for_password(checked_password):
     capitalize = False
@@ -206,19 +142,20 @@ def login():
         login_name = request.form['login']
         password = request.form['password']
         if len(login_name) >= 5 and len(password) >= 8 and rules_for_password(password):
+            session['login_name'] = login_name
             return redirect('/users')
         else:
-            abort(400, 'Missed data')
+            # return render_template('login/login.html'), 400
+            abort(400, 'Missed data')  ## or new temolate ??
     else:
-        return f'''
-        <form method="POST" action="/login">
-            <label for="login">Login:</label>
-            <input type="login" name="login" id="login"><br><br>
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password"><br><br>
-            <input type="submit" value="submit">
-        </form>
-        '''
+        return render_template('login/login.html'), 200
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return render_template('login/login.html'), 200
 
 
 @app.errorhandler(404)
